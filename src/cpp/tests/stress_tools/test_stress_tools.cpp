d1269bf03c36e99b2320cb57d25bdfa644bf65ac
Nathan Andrew Miller <nathanm@lanl.gov>
2019-12-03 12:10:00 -0700

    Completed adding objective test for linearViscoelasticity

--- src/cpp/tests/stress_tools/test_stress_tools.cpp
+++ src/cpp/tests/stress_tools/test_stress_tools.cpp
@@ -97,7 +97,7 @@ int testLinearViscoelasticity(std::ofstream &results){
     }
 
     //!Test to make sure the state variable evolution is occurring 
-    //!as expected for very large dt and alpha=0
+    //!as expected for very large dt and alpha=0 (fully implicit)
 
     currentTime = 1e10;
 
@@ -128,6 +128,55 @@ int testLinearViscoelasticity(std::ofstream &results){
             return 1;
         }
     }
+
+    //!Test for frame invariance
+    floatVector Q = {-0.44956296, -0.88488713, -0.12193405,
+                     -0.37866166,  0.31242661, -0.87120891,
+                      0.80901699, -0.3454915 , -0.47552826};
+
+    //!Rotate the previous strain
+    floatVector rotatedPreviousStrain(currentStrain.size(), 0);
+    constitutiveTools::rotateMatrix(previousStrain, Q, rotatedPreviousStrain);
+
+    //!Rotate the current strain
+    floatVector rotatedCurrentStrain(currentStrain.size(), 0);
+    constitutiveTools::rotateMatrix(currentStrain, Q, rotatedCurrentStrain);
+
+    //!Rotate the previous state variables
+    floatVector rotatedPreviousStateVariables(previousStateVariables.size(), 0);
+    for (unsigned int i=0; i<previousStateVariables.size()/dim; i++){
+        std::vector< unsigned int > indices(dim, 0);
+        floatVector subv, rotatedSubv;
+
+        for (unsigned int j=0; j<dim; j++){
+            indices[j] = i*dim + j;
+        }
+
+        vectorTools::getValuesByIndex(previousStateVariables, indices, subv);
+        constitutiveTools::rotateMatrix(subv, Q, rotatedSubv);
+        rotatedPreviousStateVariables = vectorTools::appendVectors({rotatedPreviousStateVariables, rotatedSubv});
+    }
+
+    currentTime = 23.8;
+
+    floatVector rotatedStress;
+    floatVector rotatedCurrentStateVariables;
+
+    stressTools::linearViscoelasticity( currentTime,  rotatedCurrentStrain,
+                                       previousTime, rotatedPreviousStrain,
+                                       rotatedPreviousStateVariables,
+                                       materialParameters, alpha,
+                                       rotatedStress, rotatedCurrentStateVariables);
+
+    //Calculate the initial values
+    stressTools::linearViscoelasticity( currentTime,  currentStrain,
+                                       previousTime, previousStrain,
+                                       previousStateVariables,
+                                       materialParameters, alpha,
+                                       stress, currentStateVariables);
+
+    std::cout << "rotatedStress: "; vectorTools::print(rotatedStress);
+    std::cout << "stress: "; vectorTools::print(stress);
 
     results << "testLinearViscoelasticity & True\n";
     return 0;
