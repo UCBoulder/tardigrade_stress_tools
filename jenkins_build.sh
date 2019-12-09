7024e9b9c54c7c0b99c99eb334452b91fb0b1953
Kyle Andrew Brindley <kbrindley@lanl.gov>
2019-12-09 08:51:39 -0700

    BUG: corrected to vector tools dependency instead of recursive dependency

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -6,7 +6,7 @@ declare -A deprepo
 deprepo['eigen']='https://gitlab.com/libeigen/eigen.git'
 deprepo['constitutive_tools']='ssh://git@xcp-stash.lanl.gov:7999/mm/constitutive_tools.git'
 deprepo['error_tools']='ssh://git@xcp-stash.lanl.gov:7999/mm/error_tools.git'
-deprepo['stress_tools']='ssh://git@xcp-stash.lanl.gov:7999/mm/stress_tools.git'
+deprepo['vector_tools']='ssh://git@xcp-stash.lanl.gov:7999/mm/vector_tools.git'
 proxyout='proxyout.lanl.gov:8080'
 
 # Source the Intel compilers
