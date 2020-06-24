a2d029a3321a94db62f99fbfffea9ee8e2cde3de
Kyle Brindley <kbrindley@lanl.gov>
2020-06-24 16:24:49 -0600

    WIP: build script updates

--- set_vars.sh
+++ set_vars.sh
@@ -2,11 +2,6 @@
 
 # Set common shell script variables
 repo='stress_tools'
-eigen_version='3.3.7'
+tests="src/cpp/tests/${repo}"
 workdir=${PWD}
-declare -A deprepo
-deprepo['eigen']='https://gitlab.com/libeigen/eigen.git'
-deprepo['constitutive_tools']='ssh://git@xcp-stash.lanl.gov:7999/mm/constitutive_tools.git'
-deprepo['error_tools']='ssh://git@xcp-stash.lanl.gov:7999/mm/error_tools.git'
-deprepo['vector_tools']='ssh://git@xcp-stash.lanl.gov:7999/mm/vector_tools.git'
 proxyout='proxyout.lanl.gov:8080'
