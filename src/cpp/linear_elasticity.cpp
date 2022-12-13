9a69e173c4cb44511ed93e4afc7c008301e7b2f3
Kyle Brindley <kbrindley@lanl.gov>
2022-12-13 12:21:31 -0700

    MAINT: remove the misleading structure of the anisotropic component order

--- src/cpp/linear_elasticity.cpp
+++ src/cpp/linear_elasticity.cpp
@@ -25,12 +25,10 @@ namespace linearElasticity{
          *
          * \param &parameters: The tensor components of the 9x9 stiffness tensor. Vector length determines the symmetry.
          *
-         * - 21: fully anistropic \f$C_{1111}\f$, \f$C_{1112}\f$, \f$C_{1113}\f$, \f$C_{1122}\f$, \f$C_{1123}\f$, \f$C_{1133}\f$,
-         *                                        \f$C_{1212}\f$, \f$C_{1213}\f$, \f$C_{1222}\f$, \f$C_{1223}\f$, \f$C_{1233}\f$,
-         *                                                        \f$C_{1313}\f$, \f$C_{1322}\f$, \f$C_{1323}\f$, \f$C_{1333}\f$,
-         *                                                                        \f$C_{2222}\f$, \f$C_{2223}\f$, \f$C_{2233}\f$,
-         *                                                                                        \f$C_{2323}\f$, \f$C_{2333}\f$,
-         *                                                                                                        \f$C_{3333}\f$
+         * - 21: fully anistropic \f$C_{1111}\f$, \f$C_{1112}\f$, \f$C_{1113}\f$, \f$C_{1122}\f$, \f$C_{1123}\f$,
+         *   \f$C_{1133}\f$, \f$C_{1212}\f$, \f$C_{1213}\f$, \f$C_{1222}\f$, \f$C_{1223}\f$, \f$C_{1233}\f$,
+         *   \f$C_{1313}\f$, \f$C_{1322}\f$, \f$C_{1323}\f$, \f$C_{1333}\f$, \f$C_{2222}\f$, \f$C_{2223}\f$,
+         *   \f$C_{2233}\f$, \f$C_{2323}\f$, \f$C_{2333}\f$, \f$C_{3333}\f$
          * - 3: cubic \f$C_{1111}\f$, \f$C_{1112}\f$, \f$C_{2222}\f$
          *
          * \param &stiffnessTensor: The resulting stiffness tensor.
