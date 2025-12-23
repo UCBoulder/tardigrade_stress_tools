3992a2a4d991f6294bf2f777387e0ec782578611
Nathan Miller <nami2227@colorado.edu>
2025-12-23 08:42:40 -0700

    MAINT: Realized mistake and moving declarations back to header. Moving definitions from cpp to tpp

--- src/cpp/tardigrade_stress_tools.h
+++ src/cpp/tardigrade_stress_tools.h
@@ -24,6 +24,20 @@ namespace tardigradeStressTools {
     typedef tardigradeConstitutiveTools::floatVector floatVector;  //!< Define a vector of floats
     typedef tardigradeConstitutiveTools::floatMatrix floatMatrix;  //!< Define a matrix of floats
 
+    template <unsigned int dim, class stress_iterator, typename stress_type>
+    void TARDIGRADE_OPTIONAL_INLINE calculateMeanStress(const stress_iterator &stress_begin,
+                                                        const stress_iterator &stress_end, stress_type &meanStress);
+
+    template <unsigned int dim, typename stress_type, class stress_iterator>
+    stress_type TARDIGRADE_OPTIONAL_INLINE calculateMeanStress(const stress_iterator &stress_begin,
+                                                               const stress_iterator &stress_end);
+
+    template <unsigned int dim, class stress_iterator, typename stress_type, class jacobian_iterator>
+    void TARDIGRADE_OPTIONAL_INLINE calculateMeanStress(const stress_iterator &stress_begin,
+                                                        const stress_iterator &stress_end, stress_type &meanStress,
+                                                        jacobian_iterator jacobian_begin,
+                                                        jacobian_iterator jacobian_end);
+
     void TARDIGRADE_OPTIONAL_INLINE calculateMeanStress(const floatVector &stress, floatType &meanStress);
 
     floatType TARDIGRADE_OPTIONAL_INLINE calculateMeanStress(const floatVector &stress);
@@ -34,6 +48,17 @@ namespace tardigradeStressTools {
 
     void TARDIGRADE_OPTIONAL_INLINE calculateMeanStress(const floatVector &stress, floatType &meanStress,
                                                         floatVector &jacobian);
+
+    template <unsigned int dim, class stress_iterator, class deviatoric_iterator>
+    void TARDIGRADE_OPTIONAL_INLINE calculateDeviatoricStress(const stress_iterator &stress_begin,
+                                                              const stress_iterator &stress_end,
+                                                              deviatoric_iterator    deviatoric_begin,
+                                                              deviatoric_iterator    deviatoric_end);
+
+    template <unsigned int dim, class stress_iterator, class deviatoric_iterator, class jacobian_iterator>
+    void TARDIGRADE_OPTIONAL_INLINE calculateDeviatoricStress(
+        const stress_iterator &stress_begin, const stress_iterator &stress_end, deviatoric_iterator deviatoric_begin,
+        deviatoric_iterator deviatoric_end, jacobian_iterator jacobian_begin, jacobian_iterator jacobian_end);
 
     void TARDIGRADE_OPTIONAL_INLINE calculateDeviatoricStress(const floatVector &stress, floatVector &deviatoric);
 
@@ -46,6 +71,16 @@ namespace tardigradeStressTools {
     floatVector TARDIGRADE_OPTIONAL_INLINE calculateDeviatoricStress(const floatVector &stress);
 
     floatVector TARDIGRADE_OPTIONAL_INLINE calculateDeviatoricStress(const floatVector &stress, floatMatrix &jacobian);
+
+    template <unsigned int dim, class stress_iterator, typename vonMises_type>
+    void TARDIGRADE_OPTIONAL_INLINE calculateVonMisesStress(const stress_iterator &stress_begin,
+                                                            const stress_iterator &stress_end, vonMises_type &vonMises);
+
+    template <unsigned int dim, class stress_iterator, typename vonMises_type, class jacobian_iterator>
+    void TARDIGRADE_OPTIONAL_INLINE calculateVonMisesStress(const stress_iterator &stress_begin,
+                                                            const stress_iterator &stress_end, vonMises_type &vonMises,
+                                                            jacobian_iterator jacobian_begin,
+                                                            jacobian_iterator jacobian_end);
 
     void calculateVonMisesStress(const floatVector &stress, floatType &vonMises);
 
--- src/cpp/tardigrade_stress_tools.tpp
+++ src/cpp/tardigrade_stress_tools.tpp
@@ -11,39 +11,4 @@
 
 namespace tardigradeStressTools {
 
-    template <unsigned int dim, class stress_iterator, typename stress_type>
-    void TARDIGRADE_OPTIONAL_INLINE calculateMeanStress(const stress_iterator &stress_begin,
-                                                        const stress_iterator &stress_end, stress_type &meanStress);
-
-    template <unsigned int dim, typename stress_type, class stress_iterator>
-    stress_type TARDIGRADE_OPTIONAL_INLINE calculateMeanStress(const stress_iterator &stress_begin,
-                                                               const stress_iterator &stress_end);
-
-    template <unsigned int dim, class stress_iterator, typename stress_type, class jacobian_iterator>
-    void TARDIGRADE_OPTIONAL_INLINE calculateMeanStress(const stress_iterator &stress_begin,
-                                                        const stress_iterator &stress_end, stress_type &meanStress,
-                                                        jacobian_iterator jacobian_begin,
-                                                        jacobian_iterator jacobian_end);
-
-    template <unsigned int dim, class stress_iterator, class deviatoric_iterator>
-    void TARDIGRADE_OPTIONAL_INLINE calculateDeviatoricStress(const stress_iterator &stress_begin,
-                                                              const stress_iterator &stress_end,
-                                                              deviatoric_iterator    deviatoric_begin,
-                                                              deviatoric_iterator    deviatoric_end);
-
-    template <unsigned int dim, class stress_iterator, class deviatoric_iterator, class jacobian_iterator>
-    void TARDIGRADE_OPTIONAL_INLINE calculateDeviatoricStress(
-        const stress_iterator &stress_begin, const stress_iterator &stress_end, deviatoric_iterator deviatoric_begin,
-        deviatoric_iterator deviatoric_end, jacobian_iterator jacobian_begin, jacobian_iterator jacobian_end);
-
-    template <unsigned int dim, class stress_iterator, typename vonMises_type>
-    void TARDIGRADE_OPTIONAL_INLINE calculateVonMisesStress(const stress_iterator &stress_begin,
-                                                            const stress_iterator &stress_end, vonMises_type &vonMises);
-
-    template <unsigned int dim, class stress_iterator, typename vonMises_type, class jacobian_iterator>
-    void TARDIGRADE_OPTIONAL_INLINE calculateVonMisesStress(const stress_iterator &stress_begin,
-                                                            const stress_iterator &stress_end, vonMises_type &vonMises,
-                                                            jacobian_iterator jacobian_begin,
-                                                            jacobian_iterator jacobian_end);
-
 }  // namespace tardigradeStressTools
